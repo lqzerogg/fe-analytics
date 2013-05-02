dd87e76790836d8eb71147d3c4b3ce490dd98c3e
wangshishuo <wangshishuo@gmail.com>
2013-05-02 17:43:21 +0800

    complete mobile

--- server/public/javascripts/mobile_performance_config.js
+++ server/public/javascripts/mobile_performance_config.js
@@ -1,29 +1,29 @@
 var config = {
 	dimensions: {
 		'country': ['fr', 'us', 'it', 'es', 'gb', 'br', 'dk', 'de', 'nl', 'no', 'au', 'be', 'se', 'ca', 'ru', 'ch', 'ie', 'at', 'pt', 'gr'],
-		'browser': ['m_chrome-all', 'm_firefox-all', 'm_android-all'],
-		'browser & version': ['m_chrome-28', 'm_chrome-27', 'm_chrome-26', 'm_chrome-25', 'm_firefox-22', 'm_firefox-21', 'm_firefox-20', 'm_firefox-19', 'm_android-5.0', 'm_android-4.2', 'm_android-4.1', 'm_android-4.0']
+		'browser': ['chrome-all', 'firefox-all', 'android-all'],
+		'browser & version': ['chrome-28', 'chrome-27', 'chrome-26', 'chrome-25', 'firefox-22', 'firefox-21', 'firefox-20', 'firefox-19', 'android-5.0', 'android-4.2', 'android-4.1', 'android-4.0']
 	},
 	pageTypes: '<optgroup label="litb">\
-						<option value="litb_m_all">litb 全部页面</option>\
-						<option value="litb_m_index">litb 首页</option>\
-						<option value="litb_m_mobile_products_category">litb 品类页</option>\
-						<option value="litb_m_mobile_list">litb List页</option>\
-						<option value="litb_m_mobile_advanced_search_result">litb 搜索结果页</option>\
-						<option value="litb_m_mobile_narrow_search">litb narrow search页</option>\
-						<option value="litb_m_product_info">litb 单品页</option>\
+						<option value="litb_all">litb 全部页面</option>\
+						<option value="litb_index">litb 首页</option>\
+						<option value="litb_mobile_products_category">litb 品类页</option>\
+						<option value="litb_mobile_list">litb List页</option>\
+						<option value="litb_mobile_advanced_search_result">litb 搜索结果页</option>\
+						<option value="litb_mobile_narrow_search">litb narrow search页</option>\
+						<option value="litb_product_info">litb 单品页</option>\
 					</optgroup>\
 					<optgroup label="mini">\
-						<option value="mini_m_all">mini 全部页面</option>\
-						<option value="mini_m_index">mini 首页</option>\
-						<option value="mini_m_mobile_products_category">mini 品类页</option>\
-						<option value="mini_m_mobile_list">mini List页</option>\
-						<option value="mini_m_mobile_advanced_search_result">mini 搜索结果页</option>\
-						<option value="mini_m_mobile_narrow_search">mini narrow search页</option>\
-						<option value="mini_m_product_info">mini 单品页</option>\
+						<option value="mini_all">mini 全部页面</option>\
+						<option value="mini_index">mini 首页</option>\
+						<option value="mini_mobile_products_category">mini 品类页</option>\
+						<option value="mini_mobile_list">mini List页</option>\
+						<option value="mini_mobile_advanced_search_result">mini 搜索结果页</option>\
+						<option value="mini_mobile_narrow_search">mini narrow search页</option>\
+						<option value="mini_product_info">mini 单品页</option>\
 					</optgroup>',
 	pageTemplates: {
-		'm_mobile_products_category': {
+		'mobile_products_category': {
 			'0': '普通分类列表',
 			'1': '一级分类入口',
 			'2': '分类入口模板',

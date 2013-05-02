60d5b4be381ffd30a53c8187067ff9f8225ebc43
wangshishuo <wangshishuo@gmail.com>
2013-05-02 17:24:15 +0800

    complete mobile

--- server/public/javascripts/mobile_performance_config.js
+++ server/public/javascripts/mobile_performance_config.js
@@ -1,51 +1,35 @@
 var config = {
 	dimensions: {
 		'country': ['fr', 'us', 'it', 'es', 'gb', 'br', 'dk', 'de', 'nl', 'no', 'au', 'be', 'se', 'ca', 'ru', 'ch', 'ie', 'at', 'pt', 'gr'],
-		'browser': ['chrome-all', 'firefox-all', 'ie-all'],
-		'browser & version': ['chrome-27', 'chrome-26', 'chrome-25', 'firefox-20', 'firefox-19', 'ie-10', 'ie-9']
+		'browser': ['m_chrome-all', 'm_firefox-all', 'm_android-all'],
+		'browser & version': ['m_chrome-28', 'm_chrome-27', 'm_chrome-26', 'm_chrome-25', 'm_firefox-22', 'm_firefox-21', 'm_firefox-20', 'm_firefox-19', 'm_android-5.0', 'm_android-4.2', 'm_android-4.1', 'm_android-4.0']
 	},
 	pageTypes: '<optgroup label="litb">\
-						<option value="litb_all">litb 全部页面</option>\
-						<option value="litb_index">litb 首页</option>\
-						<option value="litb_products_category">litb 品类页</option>\
-						<option value="litb_advanced_search_result">litb 搜索结果页</option>\
-						<option value="litb_narrow_search">litb narrow search页</option>\
-						<option value="litb_product_info">litb 单品页</option>\
+						<option value="litb_m_all">litb 全部页面</option>\
+						<option value="litb_m_index">litb 首页</option>\
+						<option value="litb_m_mobile_products_category">litb 品类页</option>\
+						<option value="litb_m_mobile_list">litb List页</option>\
+						<option value="litb_m_mobile_advanced_search_result">litb 搜索结果页</option>\
+						<option value="litb_m_mobile_narrow_search">litb narrow search页</option>\
+						<option value="litb_m_product_info">litb 单品页</option>\
 					</optgroup>\
 					<optgroup label="mini">\
-						<option value="mini_all">mini 全部页面</option>\
-						<option value="mini_index">mini 首页</option>\
-						<option value="mini_products_category">mini 品类页</option>\
-						<option value="mini_advanced_search_result">mini 搜索结果页</option>\
-						<option value="mini_narrow_search">mini narrow search页</option>\
-						<option value="mini_product_info">mini 单品页</option>\
+						<option value="mini_m_all">mini 全部页面</option>\
+						<option value="mini_m_index">mini 首页</option>\
+						<option value="mini_m_mobile_products_category">mini 品类页</option>\
+						<option value="mini_m_mobile_list">mini List页</option>\
+						<option value="mini_m_mobile_advanced_search_result">mini 搜索结果页</option>\
+						<option value="mini_m_mobile_narrow_search">mini narrow search页</option>\
+						<option value="mini_m_product_info">mini 单品页</option>\
 					</optgroup>',
 	pageTemplates: {
-		'products_category': {
-			'20': '分类入口模板(含子类入口)',
-			'21': '子分类推广模板(方/128)',
-			'22': '子分类推广模板(通/方/128/176)',
-			'23': '基础类目模板(通/方/128/176)',
-			'24': '基础类目模板(方/176/List)',
-			'27': '分类入口模板',
-			'28': '子分类推广模板(长/176/240)',
-			'29': '子分类推广模板(通/长/176/240)',
-			'30': '基础类目模板(长/176/240/List)',
-			'33': '特卖会模板(长/176/284)',
-			'34': '特卖会模板(方/176/176)'
-		},
-		'narrow_search': {
-			'25': 'Narrow Search模板(方/176/List)',
-			'26': 'Narrow Search模板(3C/方/176/List)',
-			'31': 'Narrow Search模板(长/176/240/List)',
-			'32': 'Narrow Search模板(3C/长/176/240/List)'
-		},
-		'product_info': {
-			'3': '婚纱礼服',
-			'4': '通用模板',
-			'5': '通用模板(1500)',
-			'6': '婚纱礼服-通栏',
-			'7': '通用模板(数据显示分离)'
+		'm_mobile_products_category': {
+			'0': '普通分类列表',
+			'1': '一级分类入口',
+			'2': '分类入口模板',
+			'3': '分类推广模板',
+			'4': '分类入口模板(不展开分类树)',
+			'9': 'Narrow Search入口'
 		}
 	}
 }
